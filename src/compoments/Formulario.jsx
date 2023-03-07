6502a6825e3cf651a5af2f96beb89f491ab8a258
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-07 14:15:47 +1100

    309. Como pasar datos y funciones del provider a otro componente

--- src/compoments/Formulario.jsx
+++ src/compoments/Formulario.jsx
@@ -1,7 +1,12 @@
 import { Fragment, useContext } from 'react'
 import { MARCAS, YEARS, PLANES } from '../constants'
+import CotizadorContext from '../context/CotizadorProvider'
 
 const Formulario = () => {
+
+  const { hola } = useContext(CotizadorContext)
+
+  console.log(hola)
 
   return (
     <>
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -3,6 +3,8 @@ import { createContext } from 'react'
 // para crear el context es una funcion
 const CotizadorContext = createContext()
 
+  const hola = "Hola Mundo"
+
 // esta es la funcion PROVIDER
 // EL PROVIDER es donde se define el state donde se tienen los effects y algunas funciones
 // EL PROVIDER es la fuente de los datos. Es una fiuncion normal
@@ -10,7 +12,9 @@ const CotizadorProvider = ({children}) => {
 
   return(
     <CotizadorContext.Provider
-      value={{}}
+      value={{
+        hola
+      }}
     >
       {children} {/* se refier a todos los componentes rodeados por el provider, este ha sido llamado en app.jsx*/}
     </CotizadorContext.Provider>
